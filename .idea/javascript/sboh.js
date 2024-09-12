3f7f79750bdb8a3a9bce9da8aa0e307dbb24d437
smanzini <smanzini023@student.wethinkcode.co.za>
2024-09-12 15:19:40 +0200

    Initial commit

---
+++ .idea/javascript/sboh.js
@@ -0,0 +1,9 @@
+// Function to open modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+// Function to close modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
